bc79cfa1d797970b3c0c03aff31f9f4b079e9b14
Oscar Barajas Tavares <oscar@gndx.dev>
2022-02-14 16:54:44 -0500

    :sparkles: [ADD] Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-	// mode: 'jit',
 	purge: ['./src/**/*.{html,js}'],
 	theme: {
 		colors: {
 			...colors,
 		},
 	},
-	plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')],
 };
